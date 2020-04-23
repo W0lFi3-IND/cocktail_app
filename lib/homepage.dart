cce1c1832905c472ad2ce35b5f8c34ae8fe282b5
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-04-23 17:41:07 +0530

    third commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,41 +13,45 @@ class _HomePageState extends State<HomePage> {
   var api = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
   var res;
   Map drinks;
+
   @override
   void initState() {
     super.initState();
     fetchData();
   }
-fetchData()async{
-    res = await http.get(api);
-    drinks=jsonDecode(res.body)["drinks"];
-    print(drinks.toString());
-    setState(() {
 
-    });
+  fetchData() async {
+    res = await http.get(api);
+    drinks = jsonDecode(res.body)["drinks"];
+    print(drinks.toString());
+    setState(() {});
   }
+
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Cocktail App"),
-          centerTitle:true,
-          elevation: 0.0,
-        ),
-        body: Center(
-          child: res != null ? ListView.builder(
-              itemCount: drinks.length,
-              itemBuilder:(context,index){
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: (Text("${drinks[index]["strDrink"]}",style: TextStyle(
-                  fontSize: 20
-                ),)),
-              );
-              })
-              :CircularProgressIndicator(),
-          ),
-
+      appBar: AppBar(
+        title: Text("Cocktail App"),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: res != null
+            ? ListView.builder(
+                itemCount: drinks.length,
+                itemBuilder: (context, index) {
+                  var drink = drinks[index];
+                  return ListTile(
+                    title:Text(
+                      "${drink["strDrink"]}",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    subtitle:Text("${drink["idDrink"]}") ,
+                    leading: CircleAvatar(backgroundImage: NetworkImage(drink["strDrinkThumb"]),),
+                  );
+                })
+            : CircularProgressIndicator(),
+      ),
     );
   }
 }
