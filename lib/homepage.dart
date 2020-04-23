25f195cceb2c9186a5d86ce8209017115928e3c9
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-04-23 17:32:43 +0530

    second commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -8,7 +10,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   var api = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail";
+  var res;
+  Map drinks;
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+fetchData()async{
+    res = await http.get(api);
+    drinks=jsonDecode(res.body)["drinks"];
+    print(drinks.toString());
+    setState(() {
 
+    });
+  }
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
@@ -18,16 +34,17 @@ class _HomePageState extends State<HomePage> {
           elevation: 0.0,
         ),
         body: Center(
-          child:ListView.builder(
-              itemCount: 50,
+          child: res != null ? ListView.builder(
+              itemCount: drinks.length,
               itemBuilder:(context,index){
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: (Text("Item $index",style: TextStyle(
+                child: (Text("${drinks[index]["strDrink"]}",style: TextStyle(
                   fontSize: 20
                 ),)),
               );
               })
+              :CircularProgressIndicator(),
           ),
 
     );
